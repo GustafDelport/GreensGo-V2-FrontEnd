1acaacbeb2c1cce032d4faf9ac056380ec0024ba
Gustaf Delport <gustafdelport@gmail.com>
2021-07-20 17:49:40 +0200

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -6,7 +6,7 @@ export default axios.create({
 
     //http://51.195.104.141:6578/dataPoints
 
-    baseURL: 'http://localhost:3001/dataPoints',
+    baseURL: 'http://51.195.104.141:6578/dataPoints',
     headers: {
         "Content-type" : "application/json"
     }
